3aae6ee82ea2dbadf794363226f6f1014c20ecd2
Andrew Yale <a.yale9@gmail.com>
2015-01-06 23:00:02 -0500

    save maps made

--- generateMaps.R
+++ generateMaps.R
@@ -13,8 +13,9 @@ map <- get_map(location=c(lon=NElon, lat=NElat), source = "google", maptype = "r
 # plot with location points
 ggmap(map) +
   geom_point(aes(x = lon, y = lat), data = gps,
-           alpha = .5, color="darkred", size = 2)
+           alpha = .5, color="darkred", size = 2) +
+  theme_nothing()
 
 
 
-
+ggsave(filename=paste('Test', ".png", sep=""), limitesize=FALSE)#height=50, width=40, units="cm", dpi=600)
