788f3d160134a21eb485055a0933ff6a52979c0b
Andrew Yale <a.yale9@gmail.com>
2015-01-08 21:41:03 -0500

    add 3 zoom maps

--- generateMaps.R
+++ generateMaps.R
@@ -17,6 +17,16 @@ NLat = mean(gps$lat[gps$lon > -80 && gps$lon < -65 && gps$lat > 35 && gps$lat < 
 northeastCoord = c(NLon, NLat)
 plotMap(gps,northeastCoord, 6, 'Northeast')
 
+# NewYork zoomed in
+newyorkCoord = c(-73.989496, 40.7317825)
+plotMap(gps, newyorkCoord, 13, 'NewYork')
+
+# Upstate zoomed in
+upstateCoord = c(-73.9730261, 42.922505)
+plotMap(gps, upstateCoord, 9, 'Upstate')
+
+
+
 # colorado
 coloradoCoord = c(-105.4334507, 39.5589435)
 plotMap(gps, coloradoCoord, 9, 'Colorado')
@@ -29,4 +39,8 @@ plotMap(gps, stMartinCoord , 12, 'StMartin')
 # SF zoomed out
 sanfranCoord = c(-122.2280528, 37.6410506)
 plotMap(gps, sanfranCoord, 10, 'SanFrancisco')
+
+# SF zoomed in
+sanfranCoordZoom = c(-122.4470414, 37.7686922)
+plotMap(gps, sanfranCoordZoom, 12, 'SanFranciscoZoom')
 
